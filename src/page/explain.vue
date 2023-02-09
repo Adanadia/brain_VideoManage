c364e5184c9200483f3c0f7745ae16df51d5e01d
adanadia <aditya@hodia.top>
2023-02-09 22:28:26 +0800

    Initial commit

---
+++ src/page/explain.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="fillcontain">
+        <head-top></head-top>
+        <p class="explain_text">{{msg}}</p>
+    </div>
+</template>
+
+<script>
+	import headTop from '../components/headTop'
+    export default {
+	    data(){
+	        return{
+                msg:'本项目仅用于实验室使用，不用于商业'
+            }
+        },
+    	components: {
+    		headTop,
+    	},
+        methods:{
+    	    async getData(){
+    	        let data = await getMsg()
+                console.log(data)
+                this.msg = data.msg
+            }
+
+        },
+        mounted() {
+	        this.getData()
+        }
+    }
+</script>
+
+<style lang="less">
+	@import '../style/mixin';
+	.explain_text{
+		margin-top: 20px;
+		text-align: center;
+		font-size: 20px;
+		color: #333;
+	}
+</style>
